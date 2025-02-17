73ef441cc91418b2b3ce1a35198f5752aec0324f
Balagbag01 <andreiegualan1@gmail.com>
2025-02-17 09:56:40 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
 print("Hello world!")
-print("Name: [Your Full Name]")
-print("Course: [Your Course Name]")
-print("Interests in programming: [Your Interests]")
+print("Name: Andrei Egualan")
+print("Course: Information Technology")
+print("Interests in programming: Web Design")
